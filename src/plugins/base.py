8aa1c1befbadb9c18a1b94c20cfb4a9cf79559f5
itpython <wrb199111@163.com>
2019-01-28 21:58:17 +0800

    完善agent,ssh,salt方法

--- src/plugins/base.py
+++ src/plugins/base.py
@@ -3,29 +3,46 @@ from conf import settings
 class BasePlugin(object):
 
     def __init__(self):
-        mode_list = ["SSH", "Salt", "Agent"]
-        if settings.MODE in mode_list:
+        self.mode_list = ["SSH", "Salt", "Agent"]
+        if settings.MODE in self.mode_list:
             self.mode = settings.MODE
         else:
             raise Exception("配置文件错误")
 
     def ssh(self, cmd):
-        pass
+        import paramiko
+
+        private_key = paramiko.RSAKey.from_private_key_file(settings.SSH_PRIVATE_KEY)
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+        ssh.connect(hostname=self.hostname, port=settings.SSH_PORT, username=settings.SSH_USER, pkey=private_key)
+        stdin, stdout, stderr = ssh.exec_command(cmd)
+        result = stdout.read()
+        ssh.close()
+        return result
 
     def agent(self, cmd):
-        pass
+        import subprocess
+        output = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True).communicate()
+        return output[0]
 
     def salt(self, cmd):
         pass
 
     def shell_cmd(self, cmd):
-        if self.mode == "SSH":
-            ret = self.ssh(cmd)
-        elif self.mode == "Salt":
-            ret = self.salt(cmd)
-        else:
-            ret = self.agent(cmd)
-        return ret
+        if self.mode not in self.mode_list:
+            raise Exception("settings.mode must be one of ['agent', 'salt', 'ssh']")
+        # 根据函数名(func_name)，获得函数对象
+        func = getattr(self, self.mode.lower())
+        output = func(cmd)
+        return output
+        # if self.mode == "SSH":
+        #     ret = self.ssh(cmd)
+        # elif self.mode == "Salt":
+        #     ret = self.salt(cmd)
+        # else:
+        #     ret = self.agent(cmd)
+        # return ret
 
     def execute(self):
         ret = self.shell_cmd("查看平台命令")
